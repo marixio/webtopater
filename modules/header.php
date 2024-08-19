8c98157ca2afd29c0f6d258e0bff1e079e8d6f95
marixio <119266148+marixio@users.noreply.github.com>
2024-08-18 22:34:24 -0400

    Update header.php

--- modules/header.php
+++ modules/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="../src/output.css" rel="stylesheet">
+   <link href="https://cdn.jsdelivr.net/npm/daisyui@4.12.10/dist/full.min.css" rel="stylesheet" type="text/css" />
 </head>
 <body>
 
